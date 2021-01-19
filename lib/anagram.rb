52ae0a8d2ff5f5e5701f417476f86d9dd0034461
Nate <7nater.nelson@gmail.com>
2021-01-19 16:28:31 -0700

    commit tree 888aee07ce19d0f077588c699f64747426bf0028

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        return_array = []
+        array.each do |word|
+            word_array = word.split("")
+            word_array.sort!
+            puts "Word_array "
+            puts word_array
+            compare_word_array = @word.split("")
+            compare_word_array.sort!
+            puts "Compare_Word_array " 
+            puts compare_word_array
+
+            if word_array == compare_word_array
+                return_array << word
+            end
+        end
+        puts return_array
+        return_array
+    end
+end
+
+# test = Anagram.new("ba")
+# test.match(["ab","abc","aed"])
